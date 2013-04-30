f5df2af2151ed364726837348ec75168b7a1e28f
Matthew Crouch <croucm01@mc-s083375.local>
2013-04-30 08:08:20 +0100

    Added phantomjs base driver

---
+++ lib/cucumber_salad/drivers/phantomjs.rb
@@ -0,0 +1,13 @@
+Capybara.register_driver :phantomjs do |app|
+  driver = Capybara::Poltergeist::Driver.new(app, 
+    phantomjs_options: %w(--load-images=no --local-storage-quota=0 --proxy-type=none),
+    inspector: true,
+    js_errors: true
+  )
+  Helper::Driver.register(
+    :smartphone,
+    :poltergeist,
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 6_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B141 Safari/8536.25"
+  )
+  driver
+end
